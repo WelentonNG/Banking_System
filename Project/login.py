a2c5e9f2b92c5d9cc5b7f97b5293044c77e18e32
Welenton <welenton24@gmail.com>
2025-12-27 20:57:55 -0300

    Começo do sistema de Login

--- Project/login.py
+++ Project/login.py
@@ -1,11 +1,41 @@
-Users={
-    "administrator":{
+# Mostrar data e hora de criação de usuarios 
+
+Users = {
+    "administrator": {
         "Login": "admin",
         "password": "admin123",
-    },
-    "Welenton Junior Melquides de Aguiar":{
-        "login": "Welenton",
-        "password": "03940441w@",
     }
-    
 }
+
+def criarusuarios():
+    print("\n---- Verificar usuário ----")
+    username = input("Digite o nome do usuario:  ")
+
+    if username not in Users:
+        print("Usuário não existe.")
+        opcao = input("Deseja criar este usuário? (s/n): ").lower()
+
+        if opcao != "s":
+            return
+
+        login = input("Digite o nome do novo usuário:  ")
+        password = input("Digite a senha:  ")
+
+        Users[username] = {
+            "login": login,
+            "password": password
+        }
+
+        print(f"Usuário '{username}' criado com sucesso")
+    else:
+        print("Erro: Usuário já existe")
+
+
+def fazerlogin():
+    print("\n ---- LOGIN ----")
+    username=input("Usuário:  ")
+
+
+
+
+fazerlogin()
